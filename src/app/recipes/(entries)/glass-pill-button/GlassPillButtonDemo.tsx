669ddb77c7c1d37d9382132bebea9af6d6e6b0c1
shogo arai <arai.shg717@gmail.com>
2026-03-09 17:33:10 +0900

    fig glass button

--- src/app/recipes/(entries)/glass-pill-button/GlassPillButtonDemo.tsx
+++ src/app/recipes/(entries)/glass-pill-button/GlassPillButtonDemo.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { MdPlayArrow } from "react-icons/md";
 import styles from "./GlassPillButtonDemo.module.css";
 
 export function GlassPillButtonDemo() {
@@ -7,6 +8,13 @@ export function GlassPillButtonDemo() {
     <div className={styles.wrap}>
       <button type="button" className={styles.glassPill}>
         Play
+      </button>
+      <button
+        type="button"
+        className={`${styles.glassPill} ${styles.glassPillCircle}`}
+        aria-label="再生"
+      >
+        <MdPlayArrow className={styles.glassPillIcon} aria-hidden />
       </button>
     </div>
   );
